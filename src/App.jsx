89d546aac4554de7656333ddb506c4ec8a0f3817
Nibir <nibirdeka70@gmail.com>
2025-12-26 15:26:22 +0530

    UI enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { AdminRoute } from './components/AdminRoute.jsx'
 import { MyBlogsPage } from './pages/MyBlogsPage.jsx'
 import { CreateBlogPage } from './pages/CreateBlogPage.jsx'
 import ProfilePage from './pages/Profile.jsx'
+import ContactPage from './pages/ContactUs.jsx'
 const App = () => {
   return (
     <div>
@@ -33,6 +34,7 @@ const App = () => {
           <Route path="/blog/:slug" element={<BlogPostPage />} />
           <Route path="/connect" element={<ConnectPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="/admin" element={ <AdminRoute>
                                             <AdminDashboard />
                                           </AdminRoute>} />
