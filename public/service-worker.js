6bd250c2190e24b3b068f5900d6c63ea8bbea180
Nibir <nibirdeka70@gmail.com>
2026-01-06 23:13:01 +0530

    feat: Implement service worker and update manifest for PWA support

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  console.log("Service Worker Installed");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service Worker Activated");
+});
+
+self.addEventListener("fetch", () => {
+  // Optional caching later
+});
